b13fc74a877de6b4b4eb113c577975ef13b4fa49
Ganesh B <ganeshsurfs@gmail.com>
2020-01-11 05:24:27 +0530

    Minor changes - empting unneeded codes

--- strongtypes/observer.py
+++
@@ -1,9 +0,0 @@
-import ast
-from copy import deepcopy
-
-class Observer(object):
-    def __init__(self):
-        self._observers = []
-    def bind_to(self, callback):
-        print('Bound Oberver')
-        self._observers.append(callback)
